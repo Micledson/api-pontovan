e12176494040e0ab3c23932a1f38327f16d282b6
= <micledson14@gmail.com>
2021-06-09 14:12:25 -0300

    add middleware de autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 
 import { UserController } from "./controllers/UserController";
+import { AuthMiddleware } from "./middlewares/AuthMiddleware";
 
 const userController = new UserController();
-
+const authMiddleware = new AuthMiddleware();
 const routes = Router();
 
 routes.post("/user", userController.create);
-routes.get("/user/", userController.store);
+routes.get("/user", userController.login);
 routes.get("/users", userController.findUsers);
-routes.put("/user", userController.update);
-routes.delete("/user", userController.delete);
+routes.put("/user/", authMiddleware.authenticate, userController.update);
+routes.delete("/user", authMiddleware.authenticate, userController.delete);
 
 export { routes };
