5c49e463f2e5f33a7ec16393c2383bdda9c6f2d7
= <micledson14@gmail.com>
2021-06-09 15:53:16 -0300

    usando routes como arquivo de rotas global

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,9 @@
 import { Router } from "express";
 
-import { UserController } from "./controllers/UserController";
-import { AuthMiddleware } from "./middlewares/AuthMiddleware";
+import UserRoutes from "./routes/UserRoutes";
 
-const userController = new UserController();
-const authMiddleware = new AuthMiddleware();
 const routes = Router();
 
-routes.post("/user", userController.create);
-routes.get("/user", userController.login);
-routes.get("/users", userController.findUsers);
-routes.put("/user/", authMiddleware.authenticate, userController.update);
-routes.delete("/user", authMiddleware.authenticate, userController.delete);
+routes.use(UserRoutes);
 
 export { routes };
