5392153fe74ea1fd7a3efcbdc214b146f306189a
= <micledson14@gmail.com>
2021-06-09 14:13:42 -0300

    add o login

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,8 @@
 import { getRepository, Repository } from "typeorm";
 import { User } from "../models/User";
 
+import bcrypt from "bcryptjs";
+
 class UserService {
   private userRepository: Repository<User>;
 
@@ -18,28 +20,50 @@ class UserService {
     return user;
   }
 
-  async store(email: string) {
-    const user = await this.userRepository.findOne({ email });
+  async login(email: string, password: string) {
+    const user = await this.userRepository
+      .findOne({ email })
+      .then(async function (value) {
+        if (!value) {
+          throw new Error("Authentication failed");
+        }
 
+        const isValidPassword = await bcrypt.compareSync(
+          password,
+          value.password
+        );
+
+        if (!isValidPassword) {
+          throw new Error("Authentication failed");
+        }
+
+        return value;
+      });
+
+    delete user.password;
     return user;
   }
 
   async findUsers(name: string) {
     const users = await this.userRepository.find({ name });
 
+    users.map((user) => delete user.password);
+
     return users;
   }
 
   async update(id: string, name: string, email: string, password?: string) {
     const user = await this.userRepository.findOne(id);
+
     user.name = name;
     user.email = email;
     user.password = password;
 
-    await this.userRepository.save(user);
+    await this.userRepository.save(user).catch(function (value) {
+      throw new Error("Update failed");
+    });
 
     delete user.password;
-
     return user;
   }
 
