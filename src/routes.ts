3587c4f6ece622844cd08af938534e788c9d69d2
= <micledson14@gmail.com>
2021-06-21 17:22:01 -0300

    separation of routes into different files

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
 import UserRoutes from "./routes/UserRoutes";
+import GroupRoutes from "./routes/GroupRoutes";
+import MessageRoutes from "./routes/MessageRoutes";
 
 const routes = Router();
 
+routes.use(MessageRoutes);
+routes.use(GroupRoutes);
 routes.use(UserRoutes);
 
 export { routes };
